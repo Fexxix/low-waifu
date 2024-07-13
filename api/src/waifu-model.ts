74af77d3e5fa21c424935b0b3d4a0eacd61d6168
Fexxix <fexxix@gmail.com>
2024-07-13 19:01:03 +0500

    hopefully wont have to make another change for a while

---
+++ api/src/waifu-model.ts
@@ -0,0 +1,28 @@
+// File: models/WaifuModel.ts
+
+import mongoose, { Schema, Document } from "mongoose"
+
+// Define interface for document in MongoDB
+export interface WaifuDocument extends Document {
+  previewUrl: string
+  url: string
+  width: number
+  height: number
+  dominantColor: string
+}
+
+// Define Mongoose schema for 'waifus' collection
+const WaifuSchema: Schema<WaifuDocument> = new Schema({
+  previewUrl: { type: String, required: true },
+  url: { type: String, required: true },
+  width: { type: Number, required: true },
+  height: { type: Number, required: true },
+  dominantColor: { type: String, required: true },
+})
+
+WaifuSchema.index({ url: 1 })
+
+// Compile and export model
+const WaifuModel: mongoose.Model<WaifuDocument> =
+  mongoose.models.Waifu || mongoose.model<WaifuDocument>("Waifu", WaifuSchema)
+export default WaifuModel
--- api/waifus.ts
+++ api/waifus.ts
@@ -1,9 +1,17 @@
 import sharp from "sharp"
-import axios, { AxiosError } from "axios"
+import axios from "axios"
+import { connectToDatabase } from "./src/mongodb.js"
+import WaifuModel from "./src/waifu-model.js"
+import { config } from "dotenv"
+import { UTApi } from "uploadthing/server"
+
+config()
 
 const WAIFU_API_URL = "https://api.waifu.im/search"
 const TARGET_WIDTH = 400 // Adjust as needed
 const QUALITY = 80
+
+const utapi = new UTApi()
 
 export async function GET(request: Request) {
   // CORS headers
@@ -12,6 +20,16 @@ export async function GET(request: Request) {
     "Access-Control-Allow-Methods": "GET, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type",
   })
+
+  try {
+    await connectToDatabase()
+  } catch (e) {
+    console.error(e)
+    return Response.json(
+      { error: "Failed to connect to database" },
+      { status: 500 }
+    )
+  }
 
   // Handle OPTIONS request for CORS preflight
   if (request.method === "OPTIONS") {
@@ -24,7 +42,7 @@ export async function GET(request: Request) {
 
     // Fetch images from Waifu API
     const response = await axios.get(WAIFU_API_URL, {
-      params: { limit },
+      params: { limit, gif: false },
     })
 
     const images = response.data.images
@@ -32,6 +50,13 @@ export async function GET(request: Request) {
     // Process images
     const processedImages = []
     for (const img of images) {
+      const image = await WaifuModel.findOne({ url: img.url })
+
+      if (image) {
+        processedImages.push(image)
+        continue
+      }
+
       const imageResponse = await axios.get(img.url, {
         responseType: "arraybuffer",
       })
@@ -60,19 +85,40 @@ export async function GET(request: Request) {
       const aspectRatio = img.width / img.height
       const newHeight = Math.round(TARGET_WIDTH / aspectRatio)
 
-      processedImages.push({
-        preview_url: `data:image/${outputFormat};base64,${optimizedImage.toString(
-          "base64"
-        )}`,
+      const uploadResult = await utapi.uploadFiles(
+        new File([optimizedImage], img.url, {
+          type: `image/${outputFormat}`,
+        })
+      )
+
+      await WaifuModel.create({
+        previewUrl:
+          uploadResult.data?.url ??
+          `data:image/${outputFormat};base64,${optimizedImage.toString(
+            "base64"
+          )}`,
+        url: img.url,
         width: TARGET_WIDTH,
         height: newHeight,
+        dominantColor: img.dominant_color,
+      })
+
+      processedImages.push({
+        preview_url:
+          uploadResult.data?.url ??
+          `data:image/${outputFormat};base64,${optimizedImage.toString(
+            "base64"
+          )}`,
+        width: TARGET_WIDTH,
+        height: newHeight,
+        dominant_color: img.dominant_color,
         url: img.url,
       })
     }
 
     return Response.json(processedImages, { headers })
   } catch (error) {
-    console.error("Error processing images:", error as AxiosError)
+    console.error("Error processing images:", error)
     return Response.json(
       { error: "An error occurred while processing images" },
       { status: 500, headers }
